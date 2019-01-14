7b26773d5aa658e8a6902123b5b9a25f7380f185
Rachel Wyatt <rw126@evansville.edu>
2019-01-14 21:47:14 +0000

    fixes syntax error in user table migration, create tables

--- db/migrate/20190114212728_create_users_table.rb
+++ db/migrate/20190114212728_create_users_table.rb
@@ -1,6 +1,6 @@
 class CreateUsersTable < ActiveRecord::Migration
   def change
-    create_table :users |t|
+    create_table :users do |t|
       t.string :name 
       t.string :email
       t.string :password_digest
