498b308a50e71d5c94e35414020ecfc81b980163
Rachel Wyatt <rw126@evansville.edu>
2019-01-14 21:54:25 +0000

    seed database

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+
+dave = User.create(name: "Dave", email: "dave@gmail.com", password: "password")
+sadie = User.create(name: "Sadie", email: "sadie@gmail.com", password: "password")
+
+Habit.create(description: "Meow for more food than I actually need", user_id: dave.id)
+Habit.create(description: "Greet Mom when she's home from work!", user_id: sadie.id)
