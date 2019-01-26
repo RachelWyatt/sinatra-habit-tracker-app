7e36d386e63eea69e49ec2a44c277e9cc3217b9b
Rachel Wyatt <rw126@evansville.edu>
2019-01-26 23:53:00 +0000

    create route to new habit page

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,3 +1,13 @@
 class HabitsController < ApplicationController
   
+  get '/habits/new' do 
+    erb '/habits/new'
+  end
+  
+  #post habits/:id (create new habit)
+  
+  #show route for one habit 
+  
+  #index route for all habits
+  
 end
