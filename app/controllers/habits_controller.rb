efa42564de67409086c08d31cd7dbc23444c3950
Rachel Wyatt <rw126@evansville.edu>
2019-01-27 00:14:36 +0000

    add post habits route

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -3,8 +3,18 @@ class HabitsController < ApplicationController
   get '/habits/new' do 
     erb :'/habits/new'
   end
-  
-  #post habits/:id (create new habit)
+
+  post '/habits' do
+    if !logged_in?
+      redirect '/'
+    end
+    if params[:description] != ""
+      @habit = Habit.create(description: params[:description], user_id: current_user.id)
+      redirect "/habit/#{@habit.id}"
+    else 
+      redirect "/habits/new"
+    end
+  end
   
   
   #show route for one habit 
