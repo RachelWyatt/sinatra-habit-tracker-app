89f712973b390e63fe4108860acb4e52c500ff25
Rachel Wyatt <rw126@evansville.edu>
2019-01-30 12:41:04 +0000

    update seed data to include days

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,6 @@ sadie = User.create(name: "Sadie", email: "sadie@gmail.com", password: "password
 
 Habit.create(description: "Meow for more food than I actually need", user_id: dave.id)
 Habit.create(description: "Greet Mom when she's home from work!", user_id: sadie.id)
+
+Day.create(day: "Today", habit_id: 1)
+Day.create(day: "January 31", habit_id: 2)
