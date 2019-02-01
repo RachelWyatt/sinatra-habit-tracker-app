c9bbb122e6df8ae7b9e2c3ebcefe514967c59f76
Rachel Wyatt <rw126@evansville.edu>
2019-02-01 13:25:25 +0000

    add logic to prevent login with blank user, redirect failed login back to login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,11 @@ class UsersController < ApplicationController
   
   post '/login' do
     @user = User.find_by(email: params[:email])
-    if @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id 
       redirect "/users/#{@user.id}"
     else 
-      "Invalid credentials"
-      #Redirect to login page
+      redirect "/login"
     end
   end
   
