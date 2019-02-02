cd9e1210ab24c34542c56f0871e7b7350fb94f3d
Rachel Wyatt <rw126@evansville.edu>
2019-02-02 13:04:02 +0000

    add validations for name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base 
   has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
   has_many :habits
   has_many :days, through: :habits
 
