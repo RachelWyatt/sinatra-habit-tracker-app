f96834a54cbcb87b3aebe88f524776a2d7d50423
Rachel Wyatt <rw126@evansville.edu>
2019-02-10 23:58:10 +0000

    add validation so user can't add empty dates to habits

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,68 +1,72 @@
 class HabitsController < ApplicationController
-  
+
   get "/habits" do
     @habits = Habit.all
     erb :"/habits/index"
   end
-  
-  get '/habits/new' do 
+
+  get '/habits/new' do
     erb :'/habits/new'
   end
 
   post '/habits' do
+
     redirect_if_not_logged_in
-    if params[:description] != "" 
+    if params[:description] != ""
       flash[:message] = "Your habit was successfully started!"
       @habit = Habit.create(description: params[:description], user_id: current_user.id)
       @day = Day.create(day: params[:day], habit_id: @habit.id)
       redirect "/habits/#{@habit.id}"
-    else 
+    else
       flash[:message] = "There was an error."
       redirect "/habits/new"
     end
   end
-  
+
   get "/habits/:id" do
     redirect_if_not_logged_in
      find_habit
      @day = @habit.days
     erb :'habits/show'
   end
-  
-  get "/habits/:id/edit" do 
+
+  get "/habits/:id/edit" do
     find_habit
     redirect_if_not_logged_in
       if @habit.user == current_user
         erb :"/habits/edit"
-      else 
+      else
         redirect "users/#{current_user.id}"
       end
   end
-  
+
   patch "/habits/:id" do
     find_habit
     redirect_if_not_logged_in
-      if @habit.user == current_user && params[:description] != ''
+
+      if @habit.user == current_user && params[:description] != '' && params[:day] != ""
         @habit.update(id: params[:id], description: params[:description])
         @day = Day.create(day: params[:day], habit_id: @habit.id)
+        flash[:message] = "Habit successfully updated!"
         redirect to "/habits/#{@habit.id}"
-      else 
-        redirect "users/#{current_user.id}"
-      end 
+      else
+        flash[:message] = "There was an error updating your habit. Please make sure the date section is filled in."
+        redirect "/habits/#{@habit.id}"
+      end
   end
-  
-  delete '/habits/:id' do 
+
+  delete '/habits/:id' do
     find_habit
-    if current_user == @habit.user 
+    if current_user == @habit.user
       @habit.destroy
       redirect "/"
-    else 
+    else
       redirect "/habits"
     end
   end
-  
+
   def find_habit
     @habit = Habit.find(params[:id])
-  end 
-  
+  end
+
 end
